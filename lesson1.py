c9d9f8b228e0bf6eb45d6104d746470d214cb4ac
ethanrjohn <ethanrjohn2012@gmail.com>
2025-02-01 16:27:55 +0530

    py

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+print("Ethan")
+print("Welcome to Codingal")
+name = "Ethan"
+print(name) #This is a comment
+x = 20
+y = 40
+print(x+y)
+fname = "Ethan"
+lname = " John"
+print("My name is",fname+lname) #This is My name
+print(100)
+print("Hello \nPython")
+print(1,"\n",2,"\n",3,"\n",4,"\n",5)
+import keyword
+print(keyword.kwlist)
